943917086a6e00f80daafae80b65a276fbea3a14
krlmari <krylova_m_v@inbox.ru>
2023-04-19 12:31:20 +0300

    init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import { GetStaticProps } from 'next';
+import React from 'react';
+import Layout from '@/components/common/Layout/Layout';
+import { BasePageProps } from '@/interfaces';
+
+interface IndexProps extends BasePageProps {
+    /* Page props*/
+}
+
+const Index: React.FC<IndexProps> = (props) => {
+    return (
+        <Layout meta={props.meta} header={props.header} sandwich={props.sandwich}>
+            {/* Page body */}
+        </Layout>
+    );
+};
+
+export const getStaticProps: GetStaticProps<IndexProps> = async () => {
+    return {
+        props: {
+            meta: {
+                title: 'Title',
+                description: 'description',
+                keywords: 'keywords'
+            },
+            header: {},
+            sandwich: {}
+        },
+        revalidate: 1
+    };
+};
+
+export default Index;
