f91895f7d346c4935a8d332be020bd35e220b483
Andrei <vitobravis@gmail.com>
2023-04-26 15:18:07 +0700

    add comments

--- src/helpers/translate.ts
+++ src/helpers/translate.ts
@@ -1,44 +1,29 @@
 export const translateCondition = (condition: string) => {
-    switch (condition) {
-        case "clear":
-            return "Ясно";
-        case "partly-cloudy":
-            return "Малооблачно";
-        case "cloudy":
-            return "Облачно с прояснениями";
-        case "overcast":
-            return "Пасмурно";
-        case "drizzle":
-            return "Морось";
-        case "light-rain":
-            return "Небольшой дождь";
-        case "rain":
-            return "Дождь";
-        case "moderate-rain":
-            return "Умеренно сильный дождь";
-        case "heavy-rain":
-            return "Сильный дождь";
-        case "continuous-heavy-rain":
-            return "Длительный сильный дождь";
-        case "showers":
-            return "Ливень";
-        case "wet-snow":
-            return "Дождь со снегом";
-        case "light-snow":
-            return "Небольшой снег";
-        case "snow":
-            return "Снег";
-        case "snow-showers":
-            return "Cнегопад";
-        case "hail":
-            return "Град";
-        case "thunderstorm":
-            return "Гроза";
-        case "thunderstorm-with-rain":
-            return "Дождь с грозой";
-        case "thunderstorm-with-hail":
-            return "Гроза с градом";
-        default:
-            return "";
+    const translates: Record<string, string> = {
+        clear: 'Ясно',
+        "partly-cloudy": 'Малооблачно',
+        cloudy: "Облачно с прояснениями",
+        overcast: "Пасмурно",
+        drizzle: "Морось",
+        "light-rain": "Небольшой дождь",
+        rain: "Дождь",
+        "moderate-rain": "Умеренно сильный дождь",
+        "heavy-rain": "Сильный дождь",
+        "continuous-heavy-rain": "Длительный сильный дождь",
+        showers: "Ливень",
+        "wet-snow": "Дождь со снегом",
+        "light-snow": "Небольшой снег",
+        snow: "Снег",
+        "snow-showers": "Cнегопад",
+        hail: "Град",
+        thunderstorm: "Гроза",
+        "thunderstorm-with-rain": "Дождь с грозой",
+        "thunderstorm-with-hail": "Гроза с градом"
+    }
+
+    try {
+        return translates[condition]
+    } catch (e) {
+        return ''
     }
 };
